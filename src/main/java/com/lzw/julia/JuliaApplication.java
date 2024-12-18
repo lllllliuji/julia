68e1e4292d35698af22667bb204c1580e1a24ad5
liuji <1546243245@qq.com>
2024-12-18 15:49:14 +0800

    project init. hello controller.

---
+++ src/main/java/com/lzw/julia/JuliaApplication.java
@@ -0,0 +1,13 @@
+package com.lzw.julia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuliaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuliaApplication.class, args);
+    }
+
+}
